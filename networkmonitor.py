aced8d21c3fb79c993e81e0e44880c33df062142
Bhavya Muni <bhavyamuni@outlook>
2019-05-22 18:43:11 +0530

    live updating graph with dash

--- networkmonitor.py
+++ networkmonitor.py
@@ -1,31 +1,90 @@
-# import pyspeedtest
-import matplotlib.pyplot as plt
-import matplotlib.animation as anim
-import time
-import psutil
+from psutil import *
 from collections import deque
+import time
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
-speed = deque(maxlen=21)
+download = []
+download.append(1)
+upload = []
+upload.append(1)
+x = []
+x.append(0)
+external_css = ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', {
+    'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+    'rel': 'stylesheet',
+    'integrity': 'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T',
+    'crossorigin': 'anonymous'
+}]
+
+app = dash.Dash(__name__, external_stylesheet=external_css)
+app.layout = html.Div(
+    [
+        html.H1("Internet usage", className='h1'),
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(id='graph-update', interval=1000, n_intervals=0),
+    ]
+)
 
 
-def animate(i):
-    st1 = psutil.net_io_counters()[1]
-    t1 = time.time()
-    time.sleep(1)
-    st2 = psutil.net_io_counters()[1]
-    t2 = time.time()
-    bytesrecv = (st2 - st1)/(t2 - t1)
-    mbrecv = bytesrecv/1024/1024
-    speed.append(mbrecv)
-    ax1.clear()
-    ax1.plot(speed)
-    fig.suptitle('Internet usage', fontsize=20)
-    plt.ylabel('MB/s', fontsize=12)
-    plt.grid(True)
-    ax1.set_xticklabels([])
+def get_bytes():
+    bytes_recv_last = net_io_counters().bytes_recv
+    time_last = time.time()
+    bytes_sent_last = net_io_counters().bytes_sent
+    return[bytes_recv_last, time_last, bytes_sent_last]
+
+@app.callback(Output('live-graph', 'figure'), [Input('graph-update', 'n_intervals')])
+def animateGraph(n):
+
+    b = get_bytes()
+    
+    bytes_recv_now = net_io_counters().bytes_recv
+    bytes_sent_now = net_io_counters().bytes_sent
+    time_now = time.time()
+    
+    kb_recv = ((bytes_recv_now-b[0])/(time_now-b[1]))/1024
+    kb_sent = ((bytes_sent_now-b[2])/(time_now-b[1]))/1024
+
+    upload.append(kb_sent)
+    download.append(kb_recv)
+    x.append(x[-1]+0.5)
+
+    time_last = time_now
+    bytes_recv_last = bytes_recv_now
+
+    data = go.Scatter(
+        y=list(download),
+        line=go.scatter.Line(
+            color='#42C4F7'
+        ),
+        mode='lines+markers',
+        name="Download"
+    )
+
+    data2 = go.Scatter(
+        y=list(upload),
+        line=go.scatter.Line(
+            color='#FF6961'
+        ),
+        mode='lines+markers',
+        name="Upload"
+    )
+
+    layout = go.Layout(
+        xaxis=dict(
+            range=[min(x), max(x)],
+        ),
+        yaxis=dict(
+            range=[min(download), max(download)],
+            fixedrange=False
+        )
+    )
+
+    return go.Figure(data=[data, data2], layout=layout)
 
 
-ani = anim.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+if __name__ == '__main__':
+    app.run_server(debug=True)
